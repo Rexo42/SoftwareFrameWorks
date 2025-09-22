e63ccdcf3472dc5e44b8307e1c68324f45772a8a
Thomas Marion <tmarion19@outlook.com>
2025-09-23 00:16:58 +1000

    minor codeBase fixes

--- Project_server/week4/database/db.js
+++ Project_server/week4/database/db.js
@@ -32,7 +32,7 @@ async function mongoConnect()
 async function setup()
 {
     // await db.collection('Users').deleteMany({});
-    await db.collection('Groups').deleteMany({}); // clear user db on start if needed
+    //await db.collection('Groups').deleteMany({}); // clear user db on start if needed
     const collectionNames = ["Users", "Groups"];
     try
     {
